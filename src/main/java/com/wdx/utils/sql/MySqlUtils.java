b67e579ba7f23e817dd2f44ad4373622d6bd361a
wangdexing <80002888@sf-express.com>
2018-11-27 15:14:42 +0800

    修改方法

--- src/main/java/com/wdx/utils/sql/MySqlUtils.java
+++ src/main/java/com/wdx/utils/sql/MySqlUtils.java
@@ -54,10 +54,52 @@ public class MySqlUtils {
 			logger.error("get error->" + path, e);
 		} finally {
 			// 关闭流
-			try {
-				br.close();
-			} catch (IOException e) {
-				logger.error("get error->", e);
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					logger.error("get error->", e);
+				}
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * 从类路径的文件中获取字符串
+	 *	@ReturnType	String 
+	 *	@Date	2018年11月27日	上午11:47:35
+	 *  @Param  @param path
+	 *  @Param  @return
+	 */
+	public static String getStringFromClassPath(String path){
+		BufferedReader br = null;
+		try {
+			// 读取文件流
+			URL resource = MySqlUtils.class.getResource(path);
+			br = new BufferedReader(new FileReader(new File(resource.getPath())));
+			// 获取内容
+			StringBuffer sb = new StringBuffer();
+			String content = null;
+			while ((content = br.readLine())!=null) {
+				sb.append(content);
+				sb.append("\n");
+			}
+			content = sb.toString();
+			if (StringUtils.isBlank(content)) {
+				return null;
+			}
+			return content;
+		} catch (Exception e) {
+			logger.error("get error->" + path, e);
+		} finally {
+			// 关闭流
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					logger.error("get error->", e);
+				}
 			}
 		}
 		return null;
