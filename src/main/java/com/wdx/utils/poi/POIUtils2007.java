47993c6025849ab3bcb66f3ee9b274ef45604bdc
wangdexing <80002888@sf-express.com>
2020-04-16 15:46:02 +0800

    add SXSSFWorkbook.

--- src/main/java/com/wdx/utils/poi/POIUtils2007.java
+++ src/main/java/com/wdx/utils/poi/POIUtils2007.java
@@ -21,6 +21,7 @@ import org.apache.poi.ss.usermodel.DataFormat;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -182,8 +183,12 @@ public class POIUtils2007 {
 		try {
 			// 真实的泛型类型
 			Class<? extends Object> clazz = sourceList.get(0).getClass();
+			
 			// 创建Workbook对像（对应一个xlsx文件）
-			Workbook workbook = new XSSFWorkbook();
+//			/// TODO SXSSFWorkbook占用内存低，且速度要快大约8倍
+//			Workbook workbook = new HSSFWorkbook();
+			Workbook workbook = new SXSSFWorkbook(100);
+			
 			// 全局样式，文字居中
 			CellStyle basicCellStyle = workbook.createCellStyle();
 			basicCellStyle.setAlignment(CellStyle.ALIGN_CENTER);
